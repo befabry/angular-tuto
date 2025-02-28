def1cefe5bef7fb09a072cb3dc2214af2a916562
b.fabry <b.fabry@groupeonepoint.com>
2025-02-28 17:42:35 +0100

    #5 modules: routing

--- components/src/app/app.routes.ts
+++ components/src/app/app.routes.ts
@@ -1,14 +1,24 @@
 import { Routes } from '@angular/router';
 import { ElementsModule } from './elements/elements.module';
 import { CollectionsModule } from './collections/collections.module';
+import { HomeComponent } from './home/home.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 export const routes: Routes = [
-    {
-        path: "elements",
-        loadChildren: () => ElementsModule
-    },
-    {
-        path: "collections",
-        loadChildren: () => CollectionsModule
-    }
+  {
+    path: 'elements',
+    loadChildren: () => ElementsModule,
+  },
+  {
+    path: 'collections',
+    loadChildren: () => CollectionsModule,
+  },
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  {
+    path: '**',
+    component: NotFoundComponent,
+  },
 ];
